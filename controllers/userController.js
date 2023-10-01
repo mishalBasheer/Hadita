0a9346740a0b1e39f5b237bfde1e9ad6e6ff2873
Mohammed Mishal <mishal2mk2@gmail.com>
2023-10-01 16:37:35 +0530

    pages updated final

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,27 @@
-const showLanding=(req,res)=>{
-    res.render('user/landingPage');
-}
-const showContact=(req,res)=>{
-    res.render('user/contact')
-}
+const showLanding = (req, res) => {
+  res.render("user/landingPage");
+};
+const showContact = (req, res) => {
+  res.render("user/contact");
+};
+const showServices = (req, res) => {
+  res.render("user/services");
+};
+const showAbout = (req, res) => {
+  res.render("user/about-us");
+};
+const showBlog = (req, res) => {
+  res.render("user/blogs");
+};
+const showBlogDetails = (req, res) => {
+  res.render("user/blog-details");
+};
 
-export {showLanding, showContact};
+export {
+  showLanding,
+  showContact,
+  showServices,
+  showAbout,
+  showBlog,
+  showBlogDetails,
+};
--- route/userRoutes.js
+++ route/userRoutes.js
@@ -1,9 +1,20 @@
-import express from 'express';
-import {showLanding,showContact} from '../controllers/userController.js'
+import express from "express";
+import {
+  showLanding,
+  showContact,
+  showServices,
+  showAbout,
+  showBlog,
+  showBlogDetails,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.route('/').get(showLanding);
-router.route('/contact').get(showContact);
+router.route("/").get(showLanding);
+router.route("/contact").get(showContact);
+router.route("/services").get(showServices);
+router.route("/about").get(showAbout);
+router.route("/blog").get(showBlog);
+router.route("/blog/:id").get(showBlogDetails);
 
 export default router;
