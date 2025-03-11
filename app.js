40192f39a7e4d66d2257b8c3754e33ec7c13585d
mishalBasheer <mishal2mk2@gmail.com>
2025-03-11 15:59:22 +0000

    server push

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public/')));
+app.use(express.static(path.join(__dirname, 'public')));
 // app.use(flash());
 
 app.use('/', userRoutes);
