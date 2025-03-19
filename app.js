9f976b7e9a5c9302dad7453a023a5e3bf2bb1ef1
haditatech <haditatech@gmail.com>
2025-03-19 06:33:27 +0530

    reduce loading time

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-import express from 'express';
-import logger from 'morgan';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import session from 'express-session';
-import flash from 'connect-flash';
+import express from "express";
+import logger from "morgan";
+import path from "path";
+import compression from "compression";
+import expressStaticGzip from "express-static-gzip";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import flash from "connect-flash";
 
-import userRoutes from './route/userRoutes.js';
-import adminRoutes from './route/adminRoutes.js';
-import authRoutes from './route/authRoutes.js';
-
+import userRoutes from "./route/userRoutes.js";
+import adminRoutes from "./route/adminRoutes.js";
+import authRoutes from "./route/authRoutes.js";
 
 const app = express();
 const __dirname = path.resolve();
 
-app.set('views', path.join(__dirname, 'view'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "view"));
+app.set("view engine", "ejs");
 
 // app.use(cookieParser());
 // app.use(
@@ -30,19 +31,21 @@ app.set('view engine', 'ejs');
 //   res.set('Cache-control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
 //   next();
 // });
-app.use(logger('dev'));
+app.use(compression());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/static", expressStaticGzip("public", { enableBrotli: true }));
 // app.use(flash());
 
-app.use('/', userRoutes);
-app.use('/admin', adminRoutes);
-app.use('/auth', authRoutes);
+app.use("/", userRoutes);
+app.use("/admin", adminRoutes);
+app.use("/auth", authRoutes);
 
 // 404 handling middleware
 app.use((req, res, next) => {
-    res.status(404).render('user/error-404');
+  res.status(404).render("user/error-404");
 });
 
 export default app;
